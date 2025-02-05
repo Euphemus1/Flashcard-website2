c402320e5927f442d8142efec40393338fceaefd
Euphemusi <nickvanverlaten@gmail.com>
2025-02-05 03:08:58 -0300

    passport.js update3

--- config/passport.js
+++ config/passport.js
@@ -2,26 +2,7 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/User');  // Make sure your User model is correctly defined
-
-// Local Strategy
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-}, async (email, password, done) => {
-  try {
-    const user = await User.findOne({ email });
-    if (!user) return done(null, false, { message: 'Invalid credentials' });
-
-    const isMatch = await user.comparePassword(password);
-    if (!isMatch) return done(null, false, { message: 'Invalid credentials' });
-
-    return done(null, user);
-  } catch (err) {
-    return done(err);
-  }
-}));
 
 // JWT Strategy
 const jwtOpts = {
@@ -43,5 +24,18 @@ passport.use(new JwtStrategy(jwtOpts, async (jwt_payload, done) => {
   } catch (err) {
     console.error('Passport JWT Error:', err); // Log the error
     return done(err, false);
-  } // fuck you mach
+  }
 }));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
