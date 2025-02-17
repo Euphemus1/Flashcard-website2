0d76542233311a40c9a71555a58e51d209a89121
Euphemusi <nickvanverlaten@gmail.com>
2025-02-17 17:04:12 -0300

    admin field to your User model (in models/User.js):

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
   isVerified: { type: Boolean, default: false },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
   resetPasswordToken: String,
